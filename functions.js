0672b23286841f56b159c66b3f6a9ebaea8a365e
Iffat <personal@email.com>
2026-05-15 13:49:18 +0500

    function expressions and function declarations code

---
+++ functions.js
@@ -0,0 +1,27 @@
+'strict mode';
+
+//Function declaration are hoisted, meaning they can be called before they are defined in the code. This is because the JavaScript engine reads the entire function declaration before executing any code.
+
+// const age1 = calculateAge(1990); //can call declaration before it is defined
+
+function calculateAge(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+}
+
+const age1 = calculateAge(1990);
+
+//Function Expression are anonymous functions that are assigned to a variable. They cannot be called before they are defined because they are not hoisted like function declarations.
+
+// const age2 = calculateAgeExpression(1999); //cannot call expression before it is defined
+
+const calculateAgeExpression = function(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+};
+
+const age2 = calculateAgeExpression(1999);
+
+//function output
+console.log(`Age calculated using function declaration: ${age1}`);
+console.log(`Age calculated using function expression: ${age2}`);
