564bf3aaf2f262d46d23d487ba1cb0a9469070c8
Iffat <iffatsattar6@gmail.com>
2026-07-31 12:04:25 +0500

    practice different dsas in js

--- scriptDeepObjects.js
+++ scriptDeepObjects.js
@@ -55,7 +55,18 @@ const prompt = require('prompt-sync')();
 
 // // ---------------------------------spread operaTOR ------------------------
 
-// const arr = [1,2,3]
+const arr = [1,2,3]
+const done=Object.entries(arr);
+const values=Object.values(arr)
+const keys=Object.keys(arr)
+    console.log(done);
+    console.log(Object.entries(arr));
+    console.log(values);
+    console.log(keys);
+
+// for(const [index, element] of arr.entries()){
+//     console.log(index, element)
+// }
 // const copy = [...arr,];
 // console.log(copy===arr)
 // const [favourite, ,...other] = arr
@@ -75,8 +86,12 @@ const resturant = {
    }
 };
 
-const {name, ...info} = resturant;
-console.log(info);
+console.log(Object.entries(resturant));
+console.log(Object.keys(resturant));
+console.log(Object.values(resturant));
+
+// const {name, ...info} = resturant;
+// console.log(info);
 // const ingredient = [prompt('let\'s make pasta, ingredient 1?'), prompt('ingredient 2?'), prompt('ingredient 3?')];
 
 // use spread operator to pass value to a function as an argument
@@ -93,3 +108,23 @@ console.log(info);
 // console.log(fav);       // 'Italian'
 // console.log(secondFav); // 'pizarani'
 // console.log(others);    // ['organic'] -> A brand new array box!
+
+
+// const self = {
+//     name: 'iffat'
+// };
+
+// // self.last= 'sattar';
+// const me= {
+//     self,
+// }
+// me.last= 'sattar'
+
+// console.log(me);
+
+// const self1 = 'iffat';
+// const me1 = [self1, 'sattar']
+
+// console.log(me1);
+
+// if (me.self?.name) console.log(`${me.self?.name?.last}`);
